0e825d0a18171622447ed8368b02165da7b13d42
Thinh-Tran-0902 <tranthinh5801@gmail.com>
2025-10-27 13:43:53 +0700

    add Assessment Service

--- services/assessment-service/AssessmentService.Domain/Entities/Assessment.cs
+++ services/assessment-service/AssessmentService.Domain/Entities/Assessment.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AssessmentService.Core.Entities
-{
-    public class Assessment
-    {
-        public int AssessmentId { get; set; }
-        public string CourseId { get; set; }
-        public string CreatorId { get; set; }
-        public string Title { get; set; }
-        public string? Description { get; set; }
-        public int TotalQuestions { get; set; }
-        public int DurationMinutes { get; set; }
-        public string? Status { get; set; }
-        public bool IsActive { get; set; } = true;
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+namespace AssessmentService.Domain.Entities;
 
-        public ICollection<AssessmentQuestion>? Questions { get; set; }
-        public ICollection<AssignmentAttempt>? Attempts { get; set; }
-    }
+public partial class Assessment
+{
+    public int AssessmentId { get; set; }
+
+    public string CourseId { get; set; } = null!;
+
+    public string CreatorId { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public int TotalQuestions { get; set; }
+
+    public int DurationMinutes { get; set; }
+
+    public string? Status { get; set; }
+
+    public bool? IsActive { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual ICollection<AssessmentQuestion> AssessmentQuestions { get; set; } = new List<AssessmentQuestion>();
+
+    public virtual ICollection<AssignmentAttempt> AssignmentAttempts { get; set; } = new List<AssignmentAttempt>();
 }
---
+++ services/assessment-service/AssessmentService.Infrastructure/DAO/Interfaces/IAssessmentDAO.cs
@@ -0,0 +1,13 @@
+﻿using AssessmentService.Domain.Entities;
+
+namespace AssessmentService.Infrastructure.Dao.Interfaces
+{
+    public interface IAssessmentDAO
+    {
+        Task<Assessment> GetAssessmentByIdAsync(int id);
+        Task<List<Assessment>> GetAllAssessmentsAsync();
+        Task AddAssessmentAsync(Assessment asse);
+        Task UpdateAssessmentAsync(Assessment asse);
+        Task DeleteAssessmentAsync(int id);
+    }
+}
