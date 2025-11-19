0ec59c898d89e1b31495ceadc5b0b4c233808e9a
ZzGreekzZ <wasasi241224@gmail.com>
2025-11-19 15:48:12 +0700

    Fixing DBContext

--- services/lesson-service/LessonService.APi/Controllers/LessonContextsController.cs
+++ services/lesson-service/LessonService.APi/Controllers/LessonContextsController.cs
@@ -15,7 +15,7 @@ namespace LessonService.Api.Controllers;
 [ApiVersion("1.0")]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiController]
-[Authorize(Roles = "Admin,Teacher")]
+[Authorize(Roles = "ADMIN,TEACHER")]
 public class LessonContextsController : ControllerBase
 {
     private readonly ICommandDispatcher _dispatcher;
